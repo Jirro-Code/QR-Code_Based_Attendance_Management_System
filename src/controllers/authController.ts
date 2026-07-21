aa1e49065ebcb94e6914ac001c1dddcc2568a280
Jairus <santosjirro55@gmail.com>
2026-07-21 20:37:56 +0800

    Added a login route

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,8 +2,9 @@ import {users, type NewUser} from "../db/schema.ts";
 import type {Request, Response} from "express";
 import {db} from "../db/connections.ts";
 import {v4 as uuid} from "uuid";
-import { hashPassword } from "../utils/password.ts";
+import { comparePassword, hashPassword } from "../utils/password.ts";
 import { generateToken } from "../utils/jwt.ts";
+import { eq } from "drizzle-orm";
 
 export const registerUser = async (req: Request<any, any, NewUser>, res: Response) => {
     try{
@@ -28,6 +29,38 @@ export const registerUser = async (req: Request<any, any, NewUser>, res: Respons
     }
     catch (e) {
         console.error("Error registering user:", e);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const loginUser = async (req: Request, res: Response) => {
+    try{
+        const user = await db.query.users.findFirst({
+            where: eq(users.email, req.body.email)
+        })
+        
+        if(!user){
+            return res.status(401).json({message: "Invalid Credentials"});
+        }
+        
+        const isPasswordValid = await comparePassword(req.body.password, user.password);
+        
+        if(!isPasswordValid){
+            return res.status(401).json({message: "Invalid Credentials"});
+        }
+        
+        const token = await generateToken({
+            userId: user.id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        });
+        
+        const {password, ...userWithoutPassword} = user;     
+        res.status(201).json({message: "Login successful", token, user: userWithoutPassword});
+    }
+    catch(e) {
+        console.error("Error logging in:", e);
         res.status(500).json({message: "Internal server error"});
     }
 }
--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/authController.ts";
+import { registerUser, loginUser } from "../controllers/authController.ts";
 import { validateBody } from "../middlewares/validation.ts";
 import { insertUserSchema } from "../db/schema.ts";
 import z from "zod";
 
 const router = Router();
 
+const loginSChema = z.object({
+    username: z.string().min(3, "Username must be at least 3 characters long"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+    email: z.email("Invalid email address")
+})
 
 router.post("/api/register", validateBody(insertUserSchema), registerUser);
+router.post("/api/login", validateBody(loginSChema), loginUser);
 
 export default router;
