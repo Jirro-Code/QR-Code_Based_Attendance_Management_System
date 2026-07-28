cf73842e597d1153fcc85d8bdfa29f75362e2df9
Jairus <santosjirro55@gmail.com>
2026-07-28 20:29:47 +0800

    Added a test for updating user fields

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,8 +1,6 @@
 import request from "supertest";
 import app from "../src/server.ts";
-import { v4 as uuid } from "uuid";
-import { createTestUser, clearDatabase, createMultipleUser } from "./setup/dbHelpers.ts";
-import { hashPassword } from "../src/utils/password.ts";
+import { clearDatabase, createMultipleUser } from "./setup/dbHelpers.ts";
 
 describe("user controller tests", () => {
     afterEach(async () => {
@@ -11,9 +9,7 @@ describe("user controller tests", () => {
     
     describe("GET /api/users/all", () => {
         it("should return 200 and a list of users for admin", async () => {
-            const { testUsers, testAdmins, testAdminsToken } = await createMultipleUser();
-            console.log("Created test users:", testUsers);
-            console.log("Created test admins:", testAdmins);
+            const { testAdminsToken } = await createMultipleUser();
             
             const response = await request(app)
                 .get("/api/users/all")
@@ -25,9 +21,7 @@ describe("user controller tests", () => {
     
     describe("GET /api/users/role/:role", () => {
         it("should return 200 and a list of users with the specified role for admin", async () => {
-            const { testUsers, testAdmins, testAdminsToken } = await createMultipleUser();
-            console.log("Created test users:", testUsers);
-            console.log("Created test admins:", testAdmins);
+            const { testAdminsToken } = await createMultipleUser();
             
             const response = await request(app)
                 .get("/api/users/role/user")
@@ -45,15 +39,26 @@ describe("user controller tests", () => {
     
     describe("GET /api/users/search", () => {
         it("should return 200 and a list of users matching the search term for admin", async () => {
-            const { testUsers, testAdmins, testAdminsToken } = await createMultipleUser();
-            console.log("Created test users:", testUsers);
-            console.log("Created test admins:", testAdmins);
+            const { testUsers, testAdminsToken } = await createMultipleUser();
             
             const response = await request(app)
                 .get(`/api/users/search?search=${testUsers[0]!.username}`)
                 .set("Authorization", `Bearer ${testAdminsToken[0]}`)
                 .expect(200);
             console.log("GET all users by search term Response:", response.body);
+        })
+    })
+    
+    describe("PUT /api/users/update/:id", () => {
+        it("should return 200 and the updated user for admin", async () => {
+            const { testUsers, testAdminsToken } = await createMultipleUser();
+            
+            const response = await request(app)
+                .put(`/api/users/update/${testUsers[2]!.id}`)
+                .set("Authorization", `Bearer ${testAdminsToken[0]}`)
+                .send({ username: "updateduser" })
+                .expect(200);
+            console.log("PUT update user Response:", response.body);
         })
     })
 })
